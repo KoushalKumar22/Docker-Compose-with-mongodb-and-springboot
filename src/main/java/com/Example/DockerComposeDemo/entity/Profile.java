c26bf99fbfcd3dee7d4813b4ad329c3c6676f5e1
KoushalKumar22 <koushalkumar2224@gmail.com>
2024-09-10 23:57:21 +0530

    Entity

--- src/main/java/com/Example/DockerComposeDemo/entity/Profile.java
+++ src/main/java/com/Example/DockerComposeDemo/entity/Profile.java
@@ -3,7 +3,44 @@ package com.Example.DockerComposeDemo.entity;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
+@Document("Profile")
 public class Profile {
 
+    @Id
+    int id;
+    String name;
+    int age;
+
+    public Profile(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Profile() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
