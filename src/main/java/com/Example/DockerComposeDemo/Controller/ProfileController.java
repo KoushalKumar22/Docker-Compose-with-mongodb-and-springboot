6292708988ce15e28ced3cc684b955228da90c97
KoushalKumar22 <koushalkumar2224@gmail.com>
2024-09-10 23:58:38 +0530

    Controller

--- src/main/java/com/Example/DockerComposeDemo/Controller/ProfileController.java
+++ src/main/java/com/Example/DockerComposeDemo/Controller/ProfileController.java
@@ -8,6 +8,30 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-
+@RestController
 public class ProfileController {
+    @Autowired
+    ProfileRepository profileRepository;
+
+    @GetMapping("/test")
+    public String test(){
+        return "This Is a Test run";
+    }
+    @PostMapping("/save")
+    public String save(@RequestBody Profile profile){
+        profileRepository.save(profile);
+        return "Data Saved";
+    }
+    @GetMapping("/all")
+    public List<Profile> findAll(){
+        return profileRepository.findAll();
+    }
+    @GetMapping("/id{id}")
+    public Optional<Profile> findById(@PathVariable int id){
+        return profileRepository.findById(id);
+    }
+    @GetMapping("/name/{name}")
+    public List<Profile> findByName(@PathVariable String name){
+        return profileRepository.findByName(name);
+    }
 }
